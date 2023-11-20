8ced228b5a43bca815047205ed94d5debb3afa64
Xiaoman Wang <62388815+renawang26@users.noreply.github.com>
2023-11-21 00:15:36 +0800

    Update function.R

--- function.R
+++ function.R
@@ -10,24 +10,6 @@ get_db_path <- function() {
     return(file.path(get_root_path(), 'data', 'repository', 'in_repository.db'))
 }
 
-get_edit_words <- function(lang, service_provider, has_score, interpret_type) {
-    
-    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
-    
-    sql = sprintf("SELECT interpret_file, IIF(word==edit_word, 0, 1) AS is_upd_num, confidence, SCORE, lang, service_provider
-                  FROM pd_interpret_words WHERE lang = '%s' AND service_provider = '%s' ", 
-                  lang, service_provider)
-    
-    if(has_score) {
-        sql = paste(sql, " AND SCORE > 0")
-    }
-    
-    words = DBI::dbGetQuery(con, sql)
-    DBI::dbDisconnect(con)
-    
-    return(words)
-}
-
 get_edit_words_similarity <- function(interpret_type) {
     
     con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
@@ -42,66 +24,4 @@ get_edit_words_similarity <- function(interpret_type) {
     
     return(words)
 }
-
-get_pvalue <- function(model_sum) {
-    return(format.pval(pf(model_sum$fstatistic[1L], model_sum$fstatistic[2L], model_sum$fstatistic[3L], lower.tail = FALSE)))
-}
-
-get_similarity_data <- function(interpret_type) {
-    
-    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
-    # get Cross-Lingual Similarity data
-    sql = "SELECT interpret_file, en_idx, zh_idx, en, zh, avg(sim) AS sim, SCORE AS score, lang, service_provider, speech, subject
-          FROM cross_lingual_similarity_all
-          WHERE SCORE > 0 AND (zh_idx-en_idx) <= 0 AND (zh_idx-en_idx) >= 0
-          GROUP BY interpret_file
-          ORDER BY interpret_file ASC"
-    
-    data = DBI::dbGetQuery(con, sql)
-    
-    DBI::dbDisconnect(con)
-    
-    return(data)
-}
-
-get_similarity_ex_data <- function(interpret_type) {
-    
-    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
-    # get Cross-Lingual Similarity data
-    sql = "SELECT * FROM cross_lingual_similarity_ex ORDER BY interpret_file ASC"
-    
-    data = DBI::dbGetQuery(con, sql)
-    
-    DBI::dbDisconnect(con)
-    
-    return(data)
-}
-
-get_interpret_files <- function(interpret_type) {
-    
-    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
-    # get Cross-Lingual Similarity data
-    sql = "SELECT interpret_file 
-                    FROM interpret
-                    WHERE service_provider = 'ibm'
-                    ORDER BY id ASC "
-    
-    data = DBI::dbGetQuery(con, sql)
-    
-    DBI::dbDisconnect(con)
-    
-    return(data)
-}
-
-flattenCorrMatrix <- function(cormat, pmat) {
-    ut <- upper.tri(cormat)
-    data.frame(
-        row = rownames(cormat)[row(cormat)[ut]],
-        column = rownames(cormat)[col(cormat)[ut]],
-        cor  =(cormat)[ut],
-        p = pmat[ut]
-    )
-}
-
-
 
