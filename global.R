6e6aa87303a8fde14ecd1f5a6de58507f73ac9db
Xiaoman Wang <62388815+renawang26@users.noreply.github.com>
2023-11-21 00:16:55 +0800

    Update global.R

--- global.R
+++ global.R
@@ -9,8 +9,6 @@ library(ggplot2)
 library(gridExtra)
 
 ROOT_PATH = 'C:/myap/PhD_project/src/InterpretRator'
-PARAVERBAL_FILE = file.path(ROOT_PATH, 'doc', 'paraverbal_symbol.xlsx')
-PARAVERBAL_ALL_FILE = file.path(ROOT_PATH, 'doc', 'paraverbal_symbol_all.xlsx')
 
 big_border = fp_border(color="black", width = 2)
 small_border = fp_border(color="black", width = 1)
@@ -25,14 +23,5 @@ my_theme <- function(x, caption, width=0.9, font_size=10) {
         hline_top(border = big_border, part = "all") %>%
         hline_bottom(border = small_border, part = "all") %>%
         hline(i = nrow(x), border = big_border, part = "body")
-    return(ft)
-}
-
-adjr_theme <- function(x, caption) {
-    ft <- my_theme(x, caption)
-    ft <- colformat_num(ft, j = c("No", "Count"), digits = 0)
-    ft <- colformat_num(ft, j = c("R.Square", "Adjusted.R.squared", "Standard.error"), digits = 3)
-    ft <- align(ft, j = c("No", "Count"), align = "center", part = "body")
-    
     return(ft)
 }
