37ca942c22e1c193d2c2ee1c78b56fd843ea107d
Xiaoman Wang <62388815+renawang26@users.noreply.github.com>
2023-11-21 00:12:28 +0800

    Update function.R

--- function.R
+++ function.R
@@ -28,18 +28,14 @@ get_edit_words <- function(lang, service_provider, has_score, interpret_type) {
     return(words)
 }
 
-get_edit_words_similarity <- function(has_score, interpret_type) {
+get_edit_words_similarity <- function(interpret_type) {
     
     con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
     
     sql = sprintf("SELECT file_name AS interpret_file, sent_id, word_id, b.word, b.word_edit, CAST(word_upd AS NUMERIC) AS is_upd_num, 
                     CAST(word_condifence AS NUMERIC) AS confidence, CAST(word_similarity AS NUMERIC) AS similarity, a.SCORE  
                              FROM pd_interpret_words a, pd_phd_file_word_confidence_similarity b
-                             WHERE a.interpret_file = b.file_name AND a.sentence_seq_no = b.sent_id AND a.word_seq_no = b.word_id ")
-    
-    if(has_score) {
-        sql = paste(sql, " AND a.SCORE > 0")
-    }
+                             WHERE a.interpret_file = b.file_name AND a.sentence_seq_no = b.sent_id AND a.word_seq_no = b.word_id AND a.SCORE > 0")
     
     words = DBI::dbGetQuery(con, sql)
     DBI::dbDisconnect(con)
