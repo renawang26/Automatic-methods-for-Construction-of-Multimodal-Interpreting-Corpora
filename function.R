ccb4400b892603afe3b8c59df1bf50d85989abfd
Xiaoman Wang <62388815+renawang26@users.noreply.github.com>
2023-12-20 23:23:11 +0800

    Update function.R

--- function.R
+++ function.R
@@ -3,16 +3,16 @@ library('sqldf')
 source('./global.R')
 
 get_root_path <- function() {
-    return('C:/myap/PhD_project/src/InterpretRator')
+    return('your own project root path')  # change to your own project root path
 }
 
 get_db_path <- function() {
-    return(file.path(get_root_path(), 'data', 'repository', 'in_repository.db'))
+    return(file.path(get_root_path(), 'your own sqlite database file')) # change to your own sqlite database file
 }
 
-get_edit_words_similarity <- function(interpret_type) {
+get_edit_words_similarity <- function() {
     
-    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path(interpret_type))
+    con <- DBI::dbConnect(RSQLite::SQLite(), get_db_path())
     
     sql = sprintf("SELECT file_name AS interpret_file, sent_id, word_id, b.word, b.word_edit, CAST(word_upd AS NUMERIC) AS is_upd_num, 
                     CAST(word_condifence AS NUMERIC) AS confidence, CAST(word_similarity AS NUMERIC) AS similarity, a.SCORE  
