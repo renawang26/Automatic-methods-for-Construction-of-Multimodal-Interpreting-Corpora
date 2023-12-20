e3f64c78a1fef35ccdc6cb91985e90916391446e
Xiaoman Wang <62388815+renawang26@users.noreply.github.com>
2023-12-20 23:40:37 +0800

    Update interpret_config_git.py

--- interpret_config_git.py
+++ interpret_config_git.py
@@ -12,19 +12,3 @@ def get_data_path():
 def get_repository_file():  # sqlite database file
     return join(get_data_path(), 'repository', 'si_repository.db')  # change to your own path
 
-# assign pause threshold in seconds
-def get_pause_threshold():
-    return 1.5
-
-# assign long pause threshold in seconds
-def get_long_pause_threshold():
-    return 3
-
-# assign tempo slow threshold, in percentage of average
-def get_tempo_slow_threshold():
-    return 2
-
-# assign tempo slow threshold, in percentage of average
-def get_tempo_quick_threshold():
-    return 0.6
-
